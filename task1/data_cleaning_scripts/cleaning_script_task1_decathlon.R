b4a009df978f9d5dcf4400076714e9f04eeed7b7
TomW49 <94124681+TomW49@users.noreply.github.com>
2022-01-06 22:26:37 +0000

    Task 4 + 1 documentation complete

--- task1/data_cleaning_scripts/cleaning_script_task1_decathlon.R
+++ task1/data_cleaning_scripts/cleaning_script_task1_decathlon.R
@@ -1,7 +1,9 @@
 library(tidyverse)
+library(here)
+library(tibble)
 
 # cleaning variables
-decathlon <- janitor::clean_names(read_rds("raw_data/decathlon.rds"))
+decathlon <- janitor::clean_names(read_rds(here("raw_data/decathlon.rds")))
 
 # creating name column for row names and standardising names
 decathlon <- decathlon %>% 
@@ -9,7 +11,7 @@ decathlon <- decathlon %>%
   mutate(name = str_to_title(name))
 
 # creating rds file of cleaned data
-write_rds(decathlon, "decathlon_cleaned.rds")
+write_rds(decathlon, "clean_data/clean_decathlon.rds")
 
 
 
